6040f8f9856a0d7fe064ed98066a207113dbaeb1
helenkuang <helen.kuang1@gmail.com>
2016-06-11 22:03:26 -0700

    Timer added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public Text TimeDisplay;
+    public float timeRemains = 60;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeRemains > 0)
+        {
+            timeRemains -= Time.deltaTime;
+            TimeDisplay.text = "" + (int) timeRemains;
+        }
+        else
+        {
+            TimeDisplay.text = "" + (int)timeRemains;
+        }
+        
+	}
+}
