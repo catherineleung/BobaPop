97ca7d264669a19ef1319a36f0f11f9e2d34ddfe
helenkuang <helen.kuang1@gmail.com>
2016-07-15 17:14:06 -0700

    Additional boba instantiation functionality added

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -7,7 +7,7 @@ public class Gameplay : MonoBehaviour {
     public GameObject[] charList;
 
     // max numbers of characters
-    public int maxCharCount = 100;
+    public int maxCharCount = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -18,29 +18,34 @@ public class Gameplay : MonoBehaviour {
             GameObject instance = Instantiate(charList[Random.Range(0, charList.Length)], new Vector3(Random.Range(-2.6f, 2.6f), 7.0f, -1.0f), Quaternion.identity) as GameObject;
         }
 
+        InvokeRepeating("addBobaHelper", 0, 6F);
+
     }
 
     //Helper for dropping new boba during gameplay
     void addBobaHelper()
     {
+        for (int i = 0; i<10; i++)
+        {
+            GameObject instance = Instantiate(charList[Random.Range(0, charList.Length)], new Vector3(Random.Range(-2.6f, 2.6f), 7.0f, -1.0f), Quaternion.identity) as GameObject;
+        }
         
     }
 
     //For actually dropping new boba
 
-    IEnumerator addBoba()
+    /*IEnumerator addBoba()
     {
         //TODO: Fix later to stop dropping after game ends
         while (true)
         {
-            addBobaHelper();
-            yield return new WaitForSeconds(5);
+            InvokeRepeating("addBobaHelper", 5, 5F);
         }
-    }
+    }*/
 	
 	// Update is called once per frame
 	void Update () {
-        
+    
 	}
 
 }
