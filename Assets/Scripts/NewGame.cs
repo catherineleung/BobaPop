601df8a6ee32fc9bfaed405de9346e21b1ca1e8f
helenkuang <helen.kuang1@gmail.com>
2016-07-22 12:37:08 -0700

    New game functionality on menu added

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        //This makes the words smaller when we press down on it
+        transform.localScale *= 0.9F;
+    }
+
+    void OnMouseUp()
+    {
+        //Application.LoadLevel(1); obsolete
+        SceneManager.LoadScene(1);
+    }
+}
