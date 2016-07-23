4178b5c85a816a37fd76667e53a7362df3675107
helenkuang <helen.kuang1@gmail.com>
2016-07-23 13:34:21 -0700

    Functionality for New Game, Main Menu, and Exit added to game over screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnMouseDown()
+    {
+        transform.localScale *= 0.9F;
+    }
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
