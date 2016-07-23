2d2a8e45550492eae41fadaafc2b2ef6194bd281
Catherine Leung <catherineleung123@gmail.com>
2016-07-22 22:42:28 -0700

    can create boba combos and bobas disappear... highlighting is not working though

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gameplay : MonoBehaviour {
+public class Gameplay : MonoBehaviour
+{
 
     // holds different GameObjects
     public GameObject[] charList;
@@ -11,9 +12,19 @@ public class Gameplay : MonoBehaviour {
 
     private GameObject selected = null;
 
+    // list to keep all bobas in combo
+    private ArrayList comboList;
+
+    // keeps all created bobas
+    private GameObject bobaPlace;
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
+        comboList = new ArrayList();
+
         for (int i = 0; i < maxCharCount; i++)
         {
             // creates a new instance of prefab object obtained from charList array
@@ -29,11 +40,11 @@ public class Gameplay : MonoBehaviour {
     //Helper for dropping new boba during gameplay
     void addBobaHelper()
     {
-        for (int i = 0; i<10; i++)
+        for (int i = 0; i < 10; i++)
         {
             GameObject instance = Instantiate(charList[Random.Range(0, charList.Length)], new Vector3(Random.Range(-2.6f, 2.6f), 7.0f, -1.0f), Quaternion.identity) as GameObject;
         }
-        
+
     }
 
     //For actually dropping new boba
@@ -46,17 +57,89 @@ public class Gameplay : MonoBehaviour {
             InvokeRepeating("addBobaHelper", 5, 5F);
         }
     }*/
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    /********* helper for conditions for combos *********/
+
+    // sets the last boba of combo as selected
+    void SetLastBobaAsSelected(bool selected)
+    {
+        BobaCharacter bobachar = GetLastBoba();
+        if (bobachar != null)
+        {
+            bobachar.SetSelected(selected);
+        }
+    }
+
+    // gets last boba of the combo
+    BobaCharacter GetLastBoba()
+    {
+        if (comboList.Count > 0)
+        {
+            return comboList[comboList.Count - 1] as BobaCharacter;
+        }
+
+        else
+        {
+            return null;
+        }
+    }
+
+    // gets all bobas allowed in combo that are around the currently selected boba and highlights them
+    void HighlightAllowedBobas(BobaCharacter bobachar)
+    {
+        foreach (BobaCharacter b in bobaPlace.GetComponentsInChildren<BobaCharacter>())
+        {
+            if (b.Check(b) && !comboList.Contains(b))
+            {
+                b.SetHighlight(true);
+                HighlightAllowedBobas(b);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            // simulate touch began
+            // touch began
+            Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(p, Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag == "bobacharacter")
+                {
+                    BobaCharacter bobachar = hit.collider.GetComponent<BobaCharacter>();
+                    if (bobachar.Check(bobachar))
+                    {
+                        comboList.Add(bobachar);
+                        bobachar.SetSelected(true);
+                    }
+
+                }
+            }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            // simulate touch ended
+            // touch ended
+            // check if combo is more than 2... if it is...
+            if (comboList.Count > 2)
+            {
+                // go through every boba in combo and delete them
+                foreach (BobaCharacter bobachar in comboList)
+                {
+                    Destroy(bobachar.gameObject);
+                }
+            }
+            else
+            {
+                // combo must be less than 2 bobas so set last boba as unselected
+                SetLastBobaAsSelected(false);
+            }
+            // clear all bobas in list
+            comboList.Clear();
         }
 
         if (Input.GetMouseButton(0))
@@ -68,19 +151,61 @@ public class Gameplay : MonoBehaviour {
             {
                 if (hit.collider.tag == "bobacharacter")
                 {
-                    if (selected != hit.collider.gameObject)
+                    BobaCharacter bobachar = hit.collider.GetComponent<BobaCharacter>();
+
+                    // if pet is not already inside combo
+                    if (!comboList.Contains(bobachar))
                     {
-                        // boba character selected
-                        if (selected != null)
+                        // is it allowed to be part of combo?
+                        if (GetLastBoba().Check(bobachar))
                         {
-                            // not selected
-                            selected.GetComponent<Animator>().SetBool("selected", false);
+                            // make last boba in combo unselected
+                            SetLastBobaAsSelected(false);
+
+                            // add it to the combo
+                            comboList.Add(bobachar);
+
+                            // make it selected
+                            bobachar.SetSelected(true);
                         }
-                        hit.collider.GetComponent<Animator>().SetBool("selected", true);
-                        selected = hit.collider.gameObject;
                     }
+
+                    else
+                    {
+                        // boba is inside combo
+                        int index = comboList.LastIndexOf(bobachar);
+
+                        if (index == comboList.Count - 2)
+                        {
+                            SetLastBobaAsSelected(false);
+                            comboList.RemoveAt(comboList.Count - 1);
+                            SetLastBobaAsSelected(true);
+                        }
+                    }
+
+                    //if (selected != hit.collider.gameObject)
+                    //{
+                    //    // boba character selected
+                    //    if (selected != null)
+                    //    {
+                    //        // not selected
+                    //        selected.GetComponent<Animator>().SetBool("selected", false);
+                    //    }
+                    //    hit.collider.GetComponent<Animator>().SetBool("selected", true);
+                    //    selected = hit.collider.gameObject;
+                    //}
                 }
             }
+        }
+
+        foreach (BobaCharacter bobachar in bobaPlace.GetComponentsInChildren<BobaCharacter>())
+        {
+            bobachar.SetHighlight(false);
+        }
+
+        if (comboList.Count > 0)
+        {
+            HighlightAllowedBobas(GetLastBoba());
         }
     }
 
