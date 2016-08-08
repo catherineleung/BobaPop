1bfc529af6c94c4f687b3185143597deebd16e2f
Catherine Leung <catherineleung123@gmail.com>
2016-08-07 22:14:51 -0700

    current score now shows after user presses restart button from pause menu

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Gameplay.score = 0;
+        Exit.scoreNum = 0;
+        PauseMenu.scoreNum = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
